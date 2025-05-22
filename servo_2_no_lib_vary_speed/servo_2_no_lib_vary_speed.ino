739fca3c357b21bb0e3727ca3f3ccfb153e64028
oscar monsalve <oscarmonsalvec5@gmail.com>
2025-05-22 09:24:58 -0500

    added servo program no Servo.h lib for varying speed

--- servo_2_no_lib_vary_speed/servo_2_no_lib_vary_speed.ino
+++ servo_2_no_lib_vary_speed/servo_2_no_lib_vary_speed.ino
@@ -1,29 +1,31 @@
 /*
-  - Positional servo (no Servo.h lib) -> Set the desired pulse width and maintain position
+  - Positional servo (no Servo.h lib) -> Control the rotational speed for a positional servo
   - Continous servo (no Servo.h lib) -> With the same code, see what happens with a continous servo
 */
 
 const int PWM_SERVO = 8;
+int pulse_width = 500;    // Starting pulse width for full speed clockwise
+int increment = 10;       // Increment in microsecond for each step
 
 void setup() {
   pinMode(PWM_SERVO, OUTPUT);
 }
 
-/**
- * Send PWM pulses to set and hold the servo position.
- *@param pulse_width Desired pulse width in microseconds.
- *@param hold_time Desired hold time in miliseconds to stop the servo.
-*/
-void hold_position(int pulse_width, int hold_time) {
-  unsigned long start_time = millis();  // Initialize the start time
-  while (millis() - start_time < hold_time) {
-    digitalWrite(PWM_SERVO, HIGH);            // Start the PWM pulse
-    delayMicroseconds(pulse_width);           // Keep it HIGH for the specified pulse width
-    digitalWrite(PWM_SERVO, LOW);             // End the pulse setting it to LOW
-    delayMicroseconds(20000 - pulse_width);   // Wait for the remainder of the 20 ms PWM signal period
-  }
-}
-
 void loop() {
+  // Send the PWM pulse
+  digitalWrite(PWM_SERVO, HIGH);           // Start the pulse
+  delayMicroseconds(pulse_width);          // Keep the pulse HIGH for the current pulse width
+  digitalWrite(PWM_SERVO, LOW);            // End the pulse (set LOW)
 
+  delayMicroseconds(20000 - pulse_width);  // Wait the ramainder of the 20ms
+
+  // Adjust the pulse width
+  pulse_width += increment;
+
+  //Reverse direction when the boundaries are reached
+  if (pulse_width >= 2500 || pulse_width <= 500){
+    increment = -increment;
+  }
+
+  delay(20);  // Increase or decrease to control the speed of transition
 }
